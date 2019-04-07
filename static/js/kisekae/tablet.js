227a70b71e83663186b8f3fd065a8f71d6930f05
misonomi <misonomi0612@gmail.com>
2019-04-07 12:51:38 +0900

    cors test

---
+++ static/js/kisekae/tablet.js
@@ -0,0 +1,15 @@
+export default {
+    name: 'tablet',
+    data() {
+        return {
+            pos: {
+                x: 0,
+                y: 0,
+            },
+            ap: 0,
+        }
+    },
+    methods: {
+
+    }
+}
