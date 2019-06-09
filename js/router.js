f33c8ebab1a40403e3bad617c5d1fe2ad78f01de
misonomi <misonomi0612@gmail.com>
2019-06-09 23:10:36 +0900

    Update router.js

--- js/router.js
+++ js/router.js
@@ -6,6 +6,7 @@ import Programs from './vue/programs.js'
 import UnderConstruction from './vue/under_construction.js'
 
 export default new VueRouter({
+    mode: 'history',
     routes: [
         {
             path: '/',
