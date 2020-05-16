e6a72dc80717fcbd44785e82fee6b79cc1531cba
misonomi <misonomi0612@gmail.com>
2020-05-16 13:32:13 +0900

    remove unnecessarry files too

--- js/kisekae/image.js
+++
@@ -1,8 +0,0 @@
-export default (url) => {
-    return new Promise((resolve, reject) => {
-        let img = new Image()
-        img.onload = () => resolve(img)
-        img.onerror = () => reject(new Error('image load failed'))
-        img.src = url
-    })
-}
