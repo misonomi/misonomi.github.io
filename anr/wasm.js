d1c4b398e6b248a22ddf8173a0cfcb160da9b998
misonomi <misonomi@users.noreply.github.com>
2020-05-18 06:44:08 +0000

    Automated deployment: Mon May 18 06:44:08 UTC 2020 5d33fcff5d72e93126a82edea5b8ed60d83b6396

--- anr/wasm.js
+++ anr/wasm.js
@@ -424,8 +424,8 @@ async function init(input) {
     imports.wbg.__wbindgen_throw = function(arg0, arg1) {
         throw new Error(getStringFromWasm0(arg0, arg1));
     };
-    imports.wbg.__wbindgen_closure_wrapper215 = function(arg0, arg1, arg2) {
-        var ret = makeMutClosure(arg0, arg1, 56, __wbg_adapter_16);
+    imports.wbg.__wbindgen_closure_wrapper209 = function(arg0, arg1, arg2) {
+        var ret = makeMutClosure(arg0, arg1, 54, __wbg_adapter_16);
         return addHeapObject(ret);
     };
 
--- top/wasm.js
+++ top/wasm.js
@@ -428,8 +428,8 @@ async function init(input) {
     imports.wbg.__wbindgen_throw = function(arg0, arg1) {
         throw new Error(getStringFromWasm0(arg0, arg1));
     };
-    imports.wbg.__wbindgen_closure_wrapper177 = function(arg0, arg1, arg2) {
-        var ret = makeMutClosure(arg0, arg1, 45, __wbg_adapter_16);
+    imports.wbg.__wbindgen_closure_wrapper176 = function(arg0, arg1, arg2) {
+        var ret = makeMutClosure(arg0, arg1, 46, __wbg_adapter_16);
         return addHeapObject(ret);
     };
 
