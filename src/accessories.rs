3daff14b26c3d9dc4ad4b7b31136744854b5dc7e
misonomi <misonomi0612@gmail.com>
2020-07-22 16:25:28 +0900

    use static str

--- src/accessories.rs
+++ src/accessories.rs
@@ -1,41 +1,39 @@
 use yew::prelude::*;
 
 #[derive(Clone)]
-pub struct AccessoryItem {
-    name: String,
-    link: String,
-    desc: String,
+pub struct AccessoryItem <'a>{
+    name: &'a str,
+    link: &'a str,
+    desc: &'a str,
 }
 
-impl AccessoryItem {
-    fn generate() -> Vec<Self> {
-        vec!(
-            AccessoryItem{
-                name: "キャス狐カーソル".to_string(),
-                link: "https://1drv.ms/u/s!Ah596hbE_QbKgwMPLJ6z6X3-X7O5".to_string(),
-                desc: "FATE/EXTRAのキャラクター，キャス狐をフィーチャーしたマウスカーソルです".to_string(),
-            },
-        )
-    }
-}
+const ITEMS: &'static Vec<AccessoryItem> = vec!(
+    AccessoryItem{
+        name: "キャス狐カーソル",
+        link: "https://1drv.ms/u/s!Ah596hbE_QbKgwMPLJ6z6X3-X7O5",
+        desc: "FATE/EXTRAのキャラクター，キャス狐をフィーチャーしたマウスカーソルです",
+    },
+    AccessoryItem{
+        name: "Bomber Cradleカーソル",
+        link: "https://1drv.ms/u/s!Ah596hbE_QbKgznFGjc2WdbTTjtV?e=l9lgL8",
+        desc: "『怒首領蜂最大往生』の戦闘機，Bomber Cradleをフィーチャーしたマウスカーソルです",
+    },
+);
 
-pub enum Msg {
-    Go(String),
+pub enum Msg <'a> {
+    Go(&'a str),
 }
 
 pub struct Accessories {
     link: ComponentLink<Self>,
-    items: Vec<AccessoryItem>
+    items: &'static Vec<AccessoryItem<'static>>
 }
 
 impl Component for Accessories {
-    type Message = Msg;
+    type Message = Msg<'static>;
     type Properties = ();
     fn create(_: Self::Properties, link: ComponentLink<Self>) -> Self {
-        Self {
-            link,
-            items: AccessoryItem::generate(),
-        }
+        Self { link, items: ITEMS}
     }
 
     fn update(&mut self, msg: Self::Message) -> ShouldRender {
@@ -57,23 +55,23 @@ impl Component for Accessories {
     fn view(&self) -> Html {
         html! {
             <main>
-                { self.items.clone().into_iter().map(|i| self.container_view(i) ).collect::<Html>() }
+                { self.items.iter().map(|i| self.container_view(&i) ).collect::<Html>() }
             </main>
         }
     }
 }
 
 impl Accessories {
-    fn container_view(&self, item: AccessoryItem) -> Html {
+    fn container_view(&self, item: &'static AccessoryItem) -> Html {
         let link =item.link;
         html! {
             <div class="container-outer">
-                <div class="container-inner" onclick=self.link.callback(move |_| Msg::Go(link.clone()))>
+                <div class="container-inner" onclick=self.link.callback(move |_| Msg::Go(link))>
                     <div class="container-title">
-                        <p class="container-text">{ &item.name }</p>
+                        <p class="container-text">{ item.name }</p>
                     </div>
                     <div class="container-disc">
-                        <p class="container-text">{ &item.desc }</p>
+                        <p class="container-text">{ item.desc }</p>
                     </div>
                 </div>
             </div>
