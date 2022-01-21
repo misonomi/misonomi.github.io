fb849f3142a8e9783c7945ac63a5ba41ec46a51a
misonomi <misonomi0612@gmail.com>
2022-01-21 16:20:34 +0900

    use cfg! instead so that compiler see the code

--- src/lib.rs
+++ src/lib.rs
@@ -9,12 +9,16 @@ mod under_construction;
 
 #[wasm_bindgen(start)]
 pub fn run_app() {
-    #[cfg(feature = "index")]
-    App::start("top", menu::init, menu::update, menu::view);
-    #[cfg(feature = "an-recruiter")]
-    App::start("anr", recruiter::init, recruiter::update, recruiter::view);
-    #[cfg(feature = "assemble")]
-    App::start("asb", under_construction::init, under_construction::update, under_construction::view);
-    #[cfg(feature = "key-detector")]
-    App::start("kdt", key_detector::init, key_detector::update, key_detector::view);
+    if cfg!(feature = "index") {
+        App::start("top", menu::init, menu::update, menu::view);
+    }
+    if cfg!(feature = "an-recruiter") {
+        App::start("anr", recruiter::init, recruiter::update, recruiter::view);
+    }
+    if cfg!(feature = "assemble") {
+        App::start("asb", under_construction::init, under_construction::update, under_construction::view);
+    }
+    if cfg!(feature = "key-detector") {
+        App::start("kdt", key_detector::init, key_detector::update, key_detector::view);
+    }
 }
