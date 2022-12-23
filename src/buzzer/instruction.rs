3a499779fe454b7eaae83891de553059b9a72638
misonomi <misonomi0612@gmail.com>
2022-12-23 18:35:43 +0900

    translate

--- src/buzzer/instruction.rs
+++ src/buzzer/instruction.rs
@@ -6,13 +6,18 @@ use crate::utils::*;
 pub fn view(lng: &Language) -> Node<Msg> {
     div![
         attrs! {At::Id => "inst-area"},
-        h2![C!["inst-title"], Multilingual::new("", "公開求人早押しクイズ", "Recruitement Buzzer Quiz").select(lng)],
+        h2![C!["inst-title"], Multilingual::new("公招计算 Buzzer Quiz", "公開求人早押しクイズ", "Recruitement Buzzer Quiz").select(lng)],
         hr![],
-        p![Multilingual::new("", "確定タグを瞬時に見極めて上級資格証をたくさん集めよう！全10問", "",).select(lng)],
+        p![Multilingual::new(
+            "Instantly discern tags and collect Distinction Certificate as much as you can! 10 questions. 帮助我翻译 (contact: https://twitter.com/miso_nomi)",
+            "確定タグを瞬時に見極めて上級資格証をたくさん集めよう！全10問",
+            "Instantly discern tags and collect Distinction Certificate as much as you can! 10 questions. Help me translate (contact: https://twitter.com/miso_nomi)",
+        )
+        .select(lng)],
         button! {
             C!["primary-button"],
             ev(Ev::Click, |_| Msg::Start),
-            Multilingual::new("", "開始", "Start").select(lng),
+            Multilingual::new("开始", "開始", "Start").select(lng),
         },
     ]
 }
